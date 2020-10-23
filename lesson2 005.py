7cc58bcb91e471a4861459dab0c3fa41dcd82eee
ayrelkins <62209059+ayrelkins@users.noreply.github.com>
2020-10-23 12:03:06 +0100

    Create lesson2 005.py

---
+++ lesson2 005.py
@@ -0,0 +1,5 @@
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+num3 = int(input("Please enter your third number: "))
+answer = (num1+num2)*num3
+print("The sum of both of your numbers and multiplyed by the last number is: ",answer)
