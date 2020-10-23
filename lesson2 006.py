6bea538051b22aa1d325204b4acb89cf8c03686a
ayrelkins <62209059+ayrelkins@users.noreply.github.com>
2020-10-23 12:06:52 +0100

    Create lesson2 006.py

---
+++ lesson2 006.py
@@ -0,0 +1,4 @@
+beginning = int(input("How many slices of pizza did you have at the beginning?: "))
+end = int(input("How many have you eaten?: "))
+answer = beginning - end
+print("This is how many slices you have left: ",answer)
