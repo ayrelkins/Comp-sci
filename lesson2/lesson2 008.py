810d51c20078579f3f04298b58112f9bfe28f67d
Itmehokiledu <62209059+ayrelkins@users.noreply.github.com>
2020-11-20 14:03:41 +0000

    Create lesson2 008.py

---
+++ lesson2/lesson2 008.py
@@ -0,0 +1,4 @@
+bill = int(input("What is the price of the bill?: "))
+people = int(input("How many have people are there?: "))
+answer = bill/people
+print("This is how much you have to pay each: ",answer)
