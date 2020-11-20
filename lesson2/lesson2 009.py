5e77b0e1f1e2949b1d123a48ef1d710378e2b14e
Itmehokiledu <62209059+ayrelkins@users.noreply.github.com>
2020-11-20 14:20:29 +0000

    Update lesson2 009.py

--- lesson2/lesson2 009.py
+++ lesson2/lesson2 009.py
@@ -1,1 +1,7 @@
-
+time = int(input("How many days?: "))
+hours = time*24
+print("This is how many hours there are: ",hours)
+minutes = hours*60
+print("This is how many minutes in your days:",minutes)
+seconds = minutes*60
+print("This is how many seconds your days have:",seconds)
