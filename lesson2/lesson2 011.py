1415bd37de142615eba09f59132583c9805d3944
Itmehokiledu <62209059+ayrelkins@users.noreply.github.com>
2020-11-20 14:41:09 +0000

    Create lesson2 011.py

---
+++ lesson2/lesson2 011.py
@@ -0,0 +1,4 @@
+bignumber = input(int("Please enter a number over 100 :"))
+smallnumber = input(int("Please enter a number under 10 :"))
+answer = bignumber/smallnumber
+print ("This is how many times smalller number fits into your larger number:",answer)
